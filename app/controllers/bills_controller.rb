aea17632606cf1e78a0a40cbba5449532fec089b
Pawel <chachako1337@gmail.com>
2018-05-22 18:19:41 +0200

    dynamic form for recipients

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -3,4 +3,24 @@ class BillsController < ApplicationController
 		@bills = Bill.all
 	end
 
+	def new
+			@bill =Bill.new
+			@bill.recipients.build
+			@bill.recipients.build
+
+		end
+
+		def create
+		
+			if Bill.create(bill_params)
+				redicrect_to actiom: :index
+			else
+				render :new
+			end
+		end
+
+		def bill_params
+			bill_params = params.require(:bill).permit(:name, :price, :description, :bank_account, recipients_attributes: [:name, :email])
+		end
+
 end
