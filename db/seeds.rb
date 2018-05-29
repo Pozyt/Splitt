f66823ad5886608fc7e1239fc2528f827974dea0
Pawel <chachako1337@gmail.com>
2018-05-29 18:12:51 +0200

    Show view

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Bill.create(name: "Pizza", price: 100)
-Bill.create(name: "Hamburger", price: 100)
-Bill.create(name: "Fish", price: 100)
-Bill.create(name: "Fries", price: 100)
-Bill.create(name: "FastFood", price: 100)
+Recipient.destroy_all
+Bill.destroy_all
+
+
+bill1 = Bill.create(name: "Pizza", price: 100, bank_account: "1234", description: "Example")
+bill2 = Bill.create(name: "Hamburger", price: 100, bank_account: "1234", description: "Example")
+bill3 = Bill.create(name: "Fish", price: 100, bank_account: "1234", description: "Example")
+
+bill1.recipients.create(name:"Paweł", email: "chachako1337@gmail.com")
+bill1.recipients.create(name:"Adam", email: "adam@gmail.com")
+
+bill2.recipients.create(name:"Marcin", email: "marcin@gmail.com")
+bill2.recipients.create(name:"Tomasz", email: "tomasz@gmail.com")
+
+bill3.recipients.create(name:"Bartosz", email: "bartosz@gmail.com")
+bill3.recipients.create(name:"Jan", email: "jan@gmail.com")
+
 
 puts 'Bills created'
